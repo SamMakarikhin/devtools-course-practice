834ba6a96cd7c142b718f75dab1f1da6bf3b25d6
Semen Makarikhin <sam20132014@mail.ru>
2020-05-11 00:54:46 +0300

    сorrect222

--- modules/odd_even_batcher_merge/test/test_odd_even_batcher_merge.cpp
+++ modules/odd_even_batcher_merge/test/test_odd_even_batcher_merge.cpp
@@ -8,51 +8,72 @@
 #include "include/odd_even_batcher_merge.h"
 
 TEST(Odd_Even_Batcher_Merge, Merge_Zero_Size_Vec) {
+  // Arrange
   std::vector<int> vec1;
   std::vector<int> vec2{ 1, 6, 12 };
 
+  // Act
   std::vector<int> vec3 = Batcher(vec1, vec2);
+
+  // Assert
   ASSERT_EQ(vec2, vec3);
 }
 
 TEST(Odd_Even_Batcher_Merge, Merge_Ordered_Vec) {
+  // Arrange
   std::vector<int> vec1{ 1, 2, 3, 4, 5, 6, 7, 8 };
   std::vector<int> vec2{ 9, 10, 11, 12, 13 };
   std::vector<int> res{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
 
+  // Act
   std::vector<int> vec3 = Batcher(vec1, vec2);
+
+  // Assert
   ASSERT_EQ(res, vec3);
 }
 
 TEST(Odd_Even_Batcher_Merge, Merge_Vec_One_Size_Elem) {
+  // Arrange
   std::vector<int> vec1{ 1 };
   std::vector<int> vec2{ 2 };
   std::vector<int> res{ 1, 2 };
 
+  // Act
   std::vector<int> vec3 = Batcher(vec1, vec2);
+
+  // Assert
   ASSERT_EQ(res, vec3);
 }
 
 TEST(Odd_Even_Batcher_Merge, Merge_Vec_With_Negative_Elem) {
+  // Arrange
   std::vector<int> vec1{ -17, -3, 5, 2 };
   std::vector<int> vec2{ -11, 3, 5, 23, 51 };
   std::vector<int> res{ -17, -11, -3, 2, 5, 3, 5, 23, 51 };
 
+  // Act
   std::vector<int> vec3 = Batcher(vec1, vec2);
+
+  // Assert
   ASSERT_EQ(res, vec3);
 }
 
 TEST(Odd_Even_Batcher_Merge, Merge_Vec_With_Repeat_Elem) {
+  // Arrange
   std::vector<int> vec1{ 1, 1, 1, 1 };
   std::vector<int> vec2{ 2, 2, 2, 2, 2 };
   std::vector<int> res{ 1, 1, 1, 1, 2, 2, 2, 2, 2 };
 
+  // Act
   std::vector<int> vec3 = Batcher(vec1, vec2);
+
+  // Assert
   ASSERT_EQ(res, vec3);
 }
 
 
 TEST(Odd_Even_Batcher_Merge, Merge_Vec_Same_Length) {
+  // Arrange
   std::mt19937 gen;
   gen.seed(static_cast<unsigned int>(time(0)));
 
@@ -67,6 +88,7 @@ TEST(Odd_Even_Batcher_Merge, Merge_Vec_Same_Length) {
     val = gen() % 100;
   }
 
+  // Act
   std::vector<int> vec3(length * 2);
   std::copy(vec1.begin(), vec1.end(), vec3.begin());
   std::copy(vec2.begin(), vec2.end(), vec3.begin() + length);
@@ -75,10 +97,13 @@ TEST(Odd_Even_Batcher_Merge, Merge_Vec_Same_Length) {
   std::sort(vec3.begin(), vec3.end());
 
   std::vector<int> res = Batcher(vec1, vec2);
+
+  // Assert
   ASSERT_EQ(res, vec3);
 }
 
 TEST(Odd_Even_Batcher_Merge, Merge_Vec_Odd_Length) {
+  // Arrange
   std::mt19937 gen;
   gen.seed(static_cast<unsigned int>(time(0)));
 
@@ -94,6 +119,7 @@ TEST(Odd_Even_Batcher_Merge, Merge_Vec_Odd_Length) {
     val = gen() % 100;
   }
 
+  // Act
   std::vector<int> vec3(length1 + length2);
   std::copy(vec1.begin(), vec1.end(), vec3.begin());
   std::copy(vec2.begin(), vec2.end(), vec3.begin() + length1);
@@ -102,10 +128,13 @@ TEST(Odd_Even_Batcher_Merge, Merge_Vec_Odd_Length) {
   std::sort(vec3.begin(), vec3.end());
 
   std::vector<int> res = Batcher(vec1, vec2);
+
+  // Assert
   ASSERT_EQ(res, vec3);
 }
 
 TEST(Odd_Even_Batcher_Merge, Merge_Vec_Even_Length) {
+  // Arrange
   std::mt19937 gen;
   gen.seed(static_cast<unsigned int>(time(0)));
 
@@ -121,6 +150,7 @@ TEST(Odd_Even_Batcher_Merge, Merge_Vec_Even_Length) {
     val = gen() % 100;
   }
 
+  // Act
   std::vector<int> vec3(length1 + length2);
   std::copy(vec1.begin(), vec1.end(), vec3.begin());
   std::copy(vec2.begin(), vec2.end(), vec3.begin() + length1);
@@ -129,10 +159,13 @@ TEST(Odd_Even_Batcher_Merge, Merge_Vec_Even_Length) {
   std::sort(vec3.begin(), vec3.end());
 
   std::vector<int> res = Batcher(vec1, vec2);
+
+  // Assert
   ASSERT_EQ(res, vec3);
 }
 
 TEST(Odd_Even_Batcher_Merge, Merge_Big_Vec) {
+  // Arrange
   std::mt19937 gen;
   gen.seed(static_cast<unsigned int>(time(0)));
 
@@ -148,6 +181,7 @@ TEST(Odd_Even_Batcher_Merge, Merge_Big_Vec) {
     val = gen() % 1000;
   }
 
+  // Act
   std::vector<int> vec3(length1 + length2);
   std::copy(vec1.begin(), vec1.end(), vec3.begin());
   std::copy(vec2.begin(), vec2.end(), vec3.begin() + length1);
@@ -156,5 +190,7 @@ TEST(Odd_Even_Batcher_Merge, Merge_Big_Vec) {
   std::sort(vec3.begin(), vec3.end());
 
   std::vector<int> res = Batcher(vec1, vec2);
+
+  // Assert
   ASSERT_EQ(res, vec3);
 }
