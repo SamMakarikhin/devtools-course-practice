e00613ba0818f6d4eaa8f1cbca84878a2956a6a2
unknown <dimentor2000@yandex.ru>
2020-05-13 02:15:35 +0300

    too many characters in string

--- modules/game-of-life/src/game-of-life-app.cpp
+++ modules/game-of-life/src/game-of-life-app.cpp
@@ -21,7 +21,8 @@ void GameOfLifeApp::help(const char* appname, const char* message) {
 }
 
 bool GameOfLifeApp::validateArguments(int argc, const char** argv) {
-  if (static_cast<uint32_t>(argc) != mainGrid.GetHeight() * mainGrid.GetWight() + 4) {
+  if (static_cast<uint32_t>(argc) != mainGrid.GetHeight()
+    * mainGrid.GetWight() + 4) {
     message_ += "ERROR: You need to fill in the grid completely\n";
     message_ += "and enter the number of steps\n";
     return false;
