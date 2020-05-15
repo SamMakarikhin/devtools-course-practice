044426b7cd5d870535f3c3a884cf9ada27ecdfd1
Semen Makarikhin <sam20132014@mail.ru>
2020-05-15 18:59:12 +0300

    corr

--- modules/odd_even_batcher_merge/src/odd_even_batcher_merge.cpp
+++ modules/odd_even_batcher_merge/src/odd_even_batcher_merge.cpp
@@ -6,10 +6,10 @@
 #include <algorithm>
 #include <vector>
 
-std::vector<int> Batcher(std::vector<int> vec1,
-  std::vector<int> vec2) {
-  vec1 = Shuffle(vec1);
-  vec2 = Shuffle(vec2);
+std::vector<int> Batcher(std::vector<int> in_vec1,
+  std::vector<int> in_vec2) {
+  std::vector<int> vec1 = Shuffle(in_vec1);
+  std::vector<int> vec2 = Shuffle(in_vec2);
   std::size_t size_odd1 = vec1.size() / 2 + vec1.size() % 2;
   std::size_t size_odd2 = vec2.size() / 2 + vec2.size() % 2;
 
