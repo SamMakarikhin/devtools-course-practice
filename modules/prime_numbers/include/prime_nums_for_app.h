710926bb6fe01a44bdc0edb4314c5723615d01be
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2020-05-16 18:13:24 +0300

    public->private

--- modules/prime_numbers/include/prime_nums_for_app.h
+++ modules/prime_numbers/include/prime_nums_for_app.h
@@ -11,9 +11,9 @@ class PrimeNums {
  public:
     PrimeNums();
     std::string operator()(int argc, const char** argv);
-    void help(const char* appname, const char* error_message);
-    bool checkArgs(int _argc, const char** _argv);
  private:
     std::string message_;
+    void help(const char* appname, const char* error_message);
+    bool checkArgs(int _argc, const char** _argv);
 };
 #endif  // MODULES_PRIME_NUMBERS_INCLUDE_PRIME_NUMS_FOR_APP_H_
