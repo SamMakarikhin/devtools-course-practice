ab7a65c365c854086cd7768257b16dcc77ef0275
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2020-05-16 12:10:13 +0300

    some coding

---
+++ modules/prime_numbers/include/prime_nums_for_app.h
@@ -0,0 +1,19 @@
+// Copyright 2020 Dobrohotov Vitaly
+
+#ifndef MODULES_PRIME_NUMBERS_INCLUDE_PRIME_NUMS_FOR_APP_H_
+#define MODULES_PRIME_NUMBERS_INCLUDE_PRIME_NUMS_FOR_APP_H_
+
+#include <string>
+#include <iostream>
+#include "include/TPrime_Nums.h"
+
+class PrimeNums {
+ public:
+    PrimeNums();
+    std::string operator()(int argc, const char** argv);
+    void help(const char* appname, const char* error_message);
+    bool checkArgs(int _argc, const char** _argv);
+ private:
+    std::string message_;
+};
+#endif  // MODULES_PRIME_NUMBERS_INCLUDE_PRIME_NUMS_FOR_APP_H_
---
+++ modules/prime_numbers/src/prime_nums_for_app.cpp
@@ -0,0 +1,61 @@
+// Copyright 2020 Dobrohotov Vitaly
+
+#include <sstream>
+#include <string>
+#include<vector>
+
+#include "include/prime_nums_for_app.h"
+#include "include/TPrime_Nums.h"
+
+PrimeNums::PrimeNums() : message_("") {}
+
+void PrimeNums::help(const char* appname, const char* error_message) {
+    message_ += std::string(error_message);
+    message_ += "This is a primary numbers application.\n";
+    message_ += "Please provide arguments in the following format:\n";
+    message_ += "<left border> <right border>\n";
+    message_ += "Where all arguments are int numbers,\n";
+    message_ += "left border less than right border,\n";
+    message_ += "and both of border not equal null";
+}
+
+bool PrimeNums::checkArgs(int argc, const char** argv) {
+    int a, b;
+    if (argc != 3) {
+        help(argv[0], "ERROR: Should be 2 arguments\n");
+        return false;
+    }
+    a = atoi(argv[1]);
+    b = atoi(argv[2]);
+    if (a < 0) {
+        help(argv[0], "ERROR: Left border can't be less than zero\n");
+        return false;
+    }
+    if (a > b) {
+        help(argv[0], "ERROR: Left border must be less than right\n");
+        return false;
+    } else if (a == 0 || b == 0) {
+        help(argv[0], "ERROR: incorrect input data\n");
+        return false;
+    }
+    return true;
+}
+
+std::string PrimeNums::operator()(int argc, const char** argv) {
+    int a;
+    int b;
+    if (!checkArgs(argc, argv)) {
+        return message_;
+    }
+    a = atoi(argv[1]);
+    b = atoi(argv[2]);
+    std::ostringstream stream;
+    TPrime_Nums pn(a, b);
+    std::vector <unsigned int> v = pn.Get_Prime_Nums();
+    message_ = "Interval: [" + std::to_string(pn.GetInterval().first) +";" +
+        std::to_string(pn.GetInterval().second) + "]\n" + "Primary numbers: ";
+    for (auto i : v) {
+       message_ += std::to_string(i) + " ";
+    }
+    return message_;
+}
