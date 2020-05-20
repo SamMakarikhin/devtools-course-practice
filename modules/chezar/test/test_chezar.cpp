cb3be1fc343b728adc0ba01b82702c06f1063fdf
Rave Guy <34068822+Rave-Guy@users.noreply.github.com>
2020-05-20 21:38:40 +0300

    codecheck

--- modules/chezar/test/test_chezar.cpp
+++ modules/chezar/test/test_chezar.cpp
@@ -1,15 +1,14 @@
 // Copyright 2020 Suchkov Makar
 
+#include "include/chezar.h"
+
 #include <gtest/gtest.h>
 
 #include <string>
 
-#include "include/chezar.h"
-
 TEST(Suchkov_Makar_Chezar_Test, Can_Decrypt_Correct_Data_Arguments) {
     // Arrange & Assert
     std::string a("fghijklmnopqrstuvwxyzabcde");
-    std::string z("abcdefghijklmnopqrstuvwxyz");
     std::string res(Decrypt(a, 5));
     // Act
     EXPECT_EQ("abcdefghijklmnopqrstuvwxyz", res);
@@ -18,7 +17,6 @@ TEST(Suchkov_Makar_Chezar_Test, Can_Decrypt_Correct_Data_Arguments) {
 TEST(Suchkov_Makar_Chezar_Test, Can_Crypt_Correct_Data_Arguments) {
     // Arrange & Assert
     std::string a("abcdefghijklmnopqrstuvwxyz");
-    std::string z("fghijklmnopqrstuvwxyzabcde");
     std::string res(Encrypt(a, 5));
     // Act
     EXPECT_EQ("fghijklmnopqrstuvwxyzabcde", res);
@@ -27,17 +25,15 @@ TEST(Suchkov_Makar_Chezar_Test, Can_Crypt_Correct_Data_Arguments) {
 TEST(Suchkov_Makar_Chezar_Test, Can_Decrypt_Correct_Data_Arguments_Upper) {
     // Arrange & Assert
     std::string a("FGHIJKLMNOPQRSTUVWXYZABCDE");
-    std::string z("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     std::string res(Decrypt(a, 5));
     // Act
-    EXPECT_EQ(z, res);
+    EXPECT_EQ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", res);
 }
 
 TEST(Suchkov_Makar_Chezar_Test, Can_Crypt_Correct_Data_Arguments_Upper) {
     // Arrange & Assert
     std::string a("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    std::string z("FGHIJKLMNOPQRSTUVWXYZABCDE");
     std::string res(Encrypt(a, 5));
     // Act
-    EXPECT_EQ(z, res);
+    EXPECT_EQ("FGHIJKLMNOPQRSTUVWXYZABCDE", res);
 }
