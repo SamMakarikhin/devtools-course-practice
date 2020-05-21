52860a4ecd2de41691d3c5849a718dcba6fe98e1
381706AnastasiaPoletueva <nastenka1193@mail.ru>
2020-05-21 17:21:44 +0300

    fix

--- modules/quick-sort/application/quick_sort.cpp
+++ modules/quick-sort/application/quick_sort.cpp
@@ -8,6 +8,6 @@
 int main(int argc, const char** argv) {
     QuickSortApp app;
     std::string output = app(argc, argv);
-    std::cout << "%s"<< std::endl << output.c_str() << std::endl;
+    std::cout << output << std::endl;
     return 0;
 }
